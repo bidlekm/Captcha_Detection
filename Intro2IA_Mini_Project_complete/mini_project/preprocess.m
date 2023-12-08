a9b4d795e58240514c5f7c4f13a47f7509284099
tomas-eg <otomaseg@outlook.com>
2023-12-08 11:37:56 +0100

    classifcation attempt

--- Intro2IA_Mini_Project_complete/mini_project/preprocess.m
+++ Intro2IA_Mini_Project_complete/mini_project/preprocess.m
@@ -1,57 +1,44 @@
 clc;clear;
 
-I = im2double(im2gray(imread("Train\captcha_0654.png")));
+%I = im2double(im2gray(imread("Train/captcha_0654.png")));
 
-fourier = fftshift(fft2(I));
-fourier2 = fftshift(fft2(I));
+%fourier = fftshift(fft2(I));
+%fourier2 = fftshift(fft2(I));
 %imagesc(log(abs(fourier)))
 %absFourier = abs(fourier);
 %maxValue = max(absFourier(:));
 %thresh_val = 0.01 * maxValue;
 %mask = absFourier < thresh_val;
 %fourierFilt = fourier .* mask;
-fourier(160,211) = mean(fourier,'all');
-fourier(157,204) = mean(fourier,'all');
-fourier(166,225) = mean(fourier,'all');
-fourier(169,232) = mean(fourier,'all');
-fourier2(160,211) = 0;
-fourier2(157,204) = 0;
-fourier2(166,225) = 0;
-fourier2(169,232) = 0;
-filteredImage = ifft2(ifftshift(fourier)) + mean2(I);
-filteredImage = mat2gray(real(filteredImage));
-filteredImage2 = ifft2(ifftshift(fourier2)) + mean2(I);
-filteredImage2 = mat2gray(real(filteredImage2));
+%fourier(160,211) = mean(fourier,'all');
+%fourier(157,204) = mean(fourier,'all');
+%fourier(166,225) = mean(fourier,'all');
+%fourier(169,232) = mean(fourier,'all');
+%fourier2(160,211) = 0;
+%fourier2(157,204) = 0;
+%fourier2(166,225) = 0;
+%fourier2(169,232) = 0;
+%filteredImage = ifft2(ifftshift(fourier)) + mean2(I);
+%filteredImage = mat2gray(real(filteredImage));
+%filteredImage2 = ifft2(ifftshift(fourier2)) + mean2(I);
+%filteredImage2 = mat2gray(real(filteredImage2));
 % imshow(fourier)
 % % crop = filteredImage(1:100, 1:100); imshow(crop); cropHist = imhist(crop);
 % % J = imgaussfilt(filteredImage,4);
 % % imshow(J);
-X1 = EM_DenoiseTV(filteredImage,0.2);
+%X1 = EM_DenoiseTV(filteredImage,0.2);
 
-X2 =  histeq(X1);
-absJ = abs(X1);
-maxValueJ = max(absJ(:));
-binaryImage = X2 < 0.09;
+%X2 =  histeq(X1);
+%absJ = abs(X1);
+%maxValueJ = max(absJ(:));
+%binaryImage = X2 < 0.09;
 
 SE = strel("disk",2);
 % for i = 1:2
 %     binaryImage = imerode(binaryImage,SE);
 % end
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imdilate(binaryImage,SE);
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imdilate(binaryImage,SE);
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imdilate(binaryImage,SE);
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imdilate(binaryImage,SE);
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imdilate(binaryImage,SE);
-binaryImage = imerode(binaryImage,SE);
-binaryImage = imdilate(binaryImage,SE);
-
-%skeleton = bwskel(binaryImage);
+original = X2
+binaryImage = imerode(original,SE);
 
 figure;
 subplot(1, 2, 1);
---
+++ Intro2IA_Mini_Project_complete/mini_project/templatematch.m
@@ -0,0 +1,13 @@
+function [ypeak,xpeak] = templatematch(original,template)
+    % Based on the approach outlined
+    %https://stackoverflow.com/questions/32664481/matlab-template-matching-using-fft
+    ox = size(original, 2); 
+    oy = size(original, 1);
+    f_o = fft2(original);
+    % Scale for cross-power spectrum calculation
+    f_t = fft2(template, oy, ox); 
+    % Calculate cross-power spectrum
+    c = real(ifft2((f_o.*conj(f_t))./abs(f_o.*f_t),'symmetric')); 
+    % Find maximum correlation and return coordinates
+    [ypeak, xpeak] = find(c == max(c(:)));
+end
